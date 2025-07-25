8223cf22b19a7628de91dcb2cd1d9d0d917bbf5d
Isidro Zertucha <isidro.zertucha@gmail.com>
2025-07-25 12:25:14 -0400

    update visual props

--- src/components/MainBoard.tsx
+++ src/components/MainBoard.tsx
@@ -61,8 +61,13 @@ const MainBoard = () => {
             <h2>Fortnite New Season</h2>
             <p>Join live stream</p>
           </div>
-          <div className="timer">
-            <span>11 : 45</span>
+          <div className="timer-wrapper">
+            <div className="timer">
+              <span>11 : 45</span>
+            </div>
+            <button className="add-friend-btn">
+              <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-user-plus"><path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="8.5" cy="7" r="4"></circle><line x1="20" y1="8" x2="20" y2="14"></line><line x1="17" y1="11" x2="23" y2="11"></line></svg>
+            </button>
           </div>
           <div className="character-container">
             <img
