06f3f8958a4c5f4af9b83b8c53d5d93dac913ad7
Isidro Zertucha <isidro.zertucha@gmail.com>
2025-07-25 12:46:22 -0400

    update visuals

--- src/components/MainBoard.tsx
+++ src/components/MainBoard.tsx
@@ -104,7 +104,12 @@ const MainBoard = () => {
           <div className="game-content-container">
             <div className="game-info">
               <span className="game-name">COD Warzone</span>
-              <span className="player-count">25</span>
+              <span className="player-count"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-sliders">
+                <line x1="3" y1="8" x2="21" y2="8"></line>
+                <circle cx="9" cy="8" r="3"></circle>
+                <line x1="3" y1="16" x2="21" y2="16"></line>
+                <circle cx="15" cy="16" r="3"></circle>
+              </svg></span>
             </div>
 
             <div className="game-separator"></div>
